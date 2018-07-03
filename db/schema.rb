aa6df5989ed33575afbe2e550925b474e18cef46
Cassy Myers <cassymyers@gmail.com>
2018-07-03 14:00:27 -0700

    add post model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_01_212849) do
+ActiveRecord::Schema.define(version: 2018_07_03_205919) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
