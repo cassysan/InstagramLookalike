741dd5fae64bb1afcffcc96c596d409f1f8048a9
Cassy Myers <cassymyers@gmail.com>
2018-07-06 10:15:41 -0700

    update index and posts css

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,33 @@
 class PostsController < ApplicationController
+  before_action :post_params, :set_post, only: [:update, :destroy]
+
   def new
     @post = Post.new
   end
 
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
   def index
+    @posts = Post.all.order('created_at DESC')
   end
 
   def show
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:description, :photo)
   end
 end
